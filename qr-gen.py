72f0dc0b543bbb085d32c32f127818fb4964246c
Tirup Mehta <183989973+TirupMehta@users.noreply.github.com>
2025-04-14 13:38:58 +0530

    Add files via upload

---
+++ qr-gen.py
@@ -0,0 +1,13 @@
+import qrcode
+
+data = "https://"  # Replace with your URL or TEXT
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,  # High error correction for logo
+    box_size=10,
+    border=4,
+)
+qr.add_data(data)
+qr.make(fit=True)
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("qr.png")
